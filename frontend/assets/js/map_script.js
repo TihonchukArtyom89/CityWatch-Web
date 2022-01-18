e9eea69b6c4647a9bb62560243486abb99cea1d3
TihonchukArtyom89 <53569237+TihonchukArtyom89@users.noreply.github.com>
2022-01-18 18:37:14 +0300

    upload stable frontend fundament for web site to hakaton 2022
    html page with script geo api

---
+++ frontend/assets/js/map_script.js
@@ -0,0 +1,14 @@
+var map_center = [47.99, 37.83];
+
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat(map_center),
+        zoom: 8
+    })
+});
