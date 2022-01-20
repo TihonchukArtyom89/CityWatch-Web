92bff7d8c7de2da2b188b276b8db5e8473bbb827
TihonchukArtyom89 <53569237+TihonchukArtyom89@users.noreply.github.com>
2022-01-20 19:05:09 +0300

    add mapon yandex api

--- frontend/assets/js/map_script.js
+++ frontend/assets/js/map_script.js
@@ -1,7 +1,7 @@
 var map_center = [37.83, 47.99];
-
+var map_center_rev = [47.99,37.83];
 var map = new ol.Map({
-    target: 'map',
+    target: 'map_OL',
     layers: [
         new ol.layer.Tile({
             source: new ol.source.OSM()
@@ -12,3 +12,10 @@ var map = new ol.Map({
         zoom: 10
     })
 });
+ymaps.ready(init);
+        function init(){
+            var myMap = new ymaps.Map("map_yandex", {
+                center: map_center_rev,
+                zoom: 7
+            });
+        }
