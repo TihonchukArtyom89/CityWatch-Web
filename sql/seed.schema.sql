140e6537db24f8846270a9c0dd42f7aeebb05e44
TihonchukArtyom89 <53569237+TihonchukArtyom89@users.noreply.github.com>
2022-02-23 12:33:34 +0300

    delete script view

--- sql/seed.schema.sql
+++ sql/seed.schema.sql
@@ -115,19 +115,3 @@ create unique clustered index cl_id_request on request(id)--кластеризо
 --Некластеризованные индексы по информационым полям 
 create unique nonclustered index noncl_info_user_profiles on user_profiles(FIO,address,district_id,role_id)
 create unique nonclustered index noncl_info_request on request(user_id,request_status_id,request_category_id)
-
---Представление"Полный профиль пользователя"
---Частично сделал подтягивание в один вид текстом ФИО, Адрес, номер телефона пользователя, роль, статус, район
---осталось добавить текстом  город 
-create view full_user_profile as
-select user.user_phone as phone,
-user_profiles.FIO as FIO,
-user_profiles.address as address,
-user_statuses.name as status,
-user_roles.name as role,
-city_districts.name as district
-from 
-users_profiles inner join user on users_profiles.id=user.id
-inner join user_roles on users_profiles.status_id=user_roles.id
-inner join user_statuses on users_profiles.role_id=user_statuses.id
-inner join city_districts on users_profiles.district_id=city_districts.id
