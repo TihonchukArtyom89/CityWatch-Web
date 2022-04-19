11d904172a95ea94d175898f5e81a3563ef368ac
TihonchukArtyom89 <53569237+TihonchukArtyom89@users.noreply.github.com>
2022-04-19 14:52:35 +0300

    Update view.schema.sql
    add city in view full_user_profile

--- sql/views/view.schema.sql
+++ sql/views/view.schema.sql
@@ -1,6 +1,6 @@
 --Представление "Полный профиль пользователя"
---Частично сделал подтягивание в один вид текстом ФИО, Адрес, номер телефона пользователя, роль, статус, район
---осталось добавить текстом  город 
+--Частично сделал подтягивание в один вид текстом ФИО, Адрес, номер телефона пользователя, роль, статус, район, город??
+--добавил город, но не уверен
 
 IF OBJECT_ID('dbo.full_user_profile', 'V') IS NOT NULL
     DROP VIEW dbo.full_user_profile
@@ -14,12 +14,13 @@ CREATE VIEW full_user_profile AS
 	,	user_statuses.name as status
 	,	user_roles.name as role
 	,	city_districts.name as district
+	,	cities.name as city
 	FROM users_profiles
 	INNER JOIN user ON users_profiles.id = user.id
 	INNER JOIN user_roles ON users_profiles.status_id=user_roles.id
 	INNER JOIN user_statuses ON users_profiles.role_id=user_statuses.id
 	INNER JOIN city_districts ON users_profiles.district_id=city_districts.id
-
+	INNER JOIN (SELECT name FROM cities WHERE id IN (SELECT city_id FROM city_districts)) ON users_profiles.district_id=city_districts.id
 GO
 
 SELECT * FROM full_user_profile
